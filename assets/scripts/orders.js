73482eae3c27fa749e47b398341205544571799f
“Ervis <ervistrupja@outlook.com>
2023-11-13 20:31:32 +0100

    added ajax request

--- assets/scripts/orders.js
+++ assets/scripts/orders.js
@@ -31,7 +31,7 @@ function populateTable(){
         const newRowHtml = `<tr>
             <td>${order.fullName}</td>
             <td>${order.email}</td>
-            <td>${order.description}</td>
+            <td onclick="getDescriptionLanguage('${order.description}')">${order.description}</td>
             <td>
                 <button id="editBtn" data-order-id="${order.id}">Edit</button>
                 <button id="removeBtn" data-order-id="${order.id}">Remove</button>
@@ -140,4 +140,37 @@ $("#updateBtn").click(function(){
     localStorage.setItem('orderToUpdateId', '')
     location.reload();
 });
+
+
+function getDescriptionLanguage(textToBeChecked){
+    console.log(textToBeChecked);
+    $.ajax({
+        async: true,
+        crossDomain: true,
+        url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'application/gzip',
+            'X-RapidAPI-Key': '4899b5a1f2msh3594b57d4f697f7p185222jsne688960537df',
+            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        },
+        data: {
+            q: textToBeChecked
+        },
+        success: function (response) {
+            console.log(response);
+        alert(`Language detected: ${response.data.detections[0][0].language}`)
+
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+    
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
+}
 
